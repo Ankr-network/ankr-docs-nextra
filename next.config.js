60465d75fd71060293653b3d80684416333b3297
Aleksandr Ishchenko <BigBadAlien@users.noreply.github.com>
2022-08-26 11:24:21 +0300

    feat: NA Build static (#2)

--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
-const isProd = process.env.NODE_ENV === 'production'
-
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.js",
   unstable_staticImage: true,
   unstable_contentDump: true,
   unstable_flexsearch: true,
-  unstable_staticImage: true,
+
 });
 module.exports = withNextra({
-  assetPrefix: isProd ? 'https://bs-1-stage.dccn.ankr.com' : undefined,
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
 });
 
 /* example of how to set up redirects
